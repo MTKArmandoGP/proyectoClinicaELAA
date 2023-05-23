d66de2545ecfc9aa9f1af90c4a690f2185f06602
belxoch12 <beltranvilledaxochitlcitlally@gmail.com>
2023-05-23 13:38:20 -0600

    Pantalla registro a medias

--- app/src/main/java/com/example/clinicaelaa_proyecto/LoginActivity.java
+++ app/src/main/java/com/example/clinicaelaa_proyecto/LoginActivity.java
@@ -2,13 +2,30 @@ package com.example.clinicaelaa_proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button registrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registrar=findViewById(R.id.btn_registrar);
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getApplicationContext(),Registro.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 }
